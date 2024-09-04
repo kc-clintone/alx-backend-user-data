7dd190f70257b44a92228a45daf42371bb0d64a0
kc-clintone <kayseeclintone@gmail.com>
2024-09-04 06:56:14 +0000

    Task 1: Error handling

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -14,6 +14,16 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
+@app_views.route("/unauthorized/", strict_slashes=False)
+def unauthorized() -> str:
+    """Route for unauthorized requests
+
+    Returns:
+        str: A 401 status code (unauthorized)
+    """
+    abort(401)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
