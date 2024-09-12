ca69ddffb9694c65fc843acfe71719d0f68d4ea2
kc-clintone <kayseeclintone@gmail.com>
2024-09-12 22:10:10 +0000

    RTask: 0    llk: 0: : User model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+An SQLAlchemy model => User
+"""
+from sqlalchemy import String, Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+
+the_base = declarative_base()
+
+
+class User(the_base):
+    """
+    How the tabke will/shall look like
+    """
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
